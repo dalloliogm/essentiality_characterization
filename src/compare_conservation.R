a2c76c0459f50d1d61b5116c905ceb7567ba2027
Giovanni M Dall'Olio <dalloliogm@gmail.com>
2015-12-25 19:02:44 +0000

    reading S3 data and calculating ranks

--- src/compare_conservation.R
+++ src/compare_conservation.R
@@ -1,5 +1,8 @@
 #!/usr/bin/env Rscript
 
+library(AnnotationHub)
+library(dplyr)
+library(tidyr)
 library(phastCons100way.UCSC.hg19)
 library(Homo.sapiens)
 
@@ -9,4 +12,14 @@ if (!exists("allgenes")) {
     allgenes$cons100way = scores (phastCons100way.UCSC.hg19, allgenes)
 }
 
+allgenes.df = as.data.frame(mcols(allgenes))
+head(allgenes.df)
+allgenes.df = allgenes.df %>%
+    arrange(desc(cons100way)) %>% 
+    mutate(rank=min_rank(cons100way))
+
+print("reading S3 data")
+mmc3 = read.csv("data/mmc3.csv")
+mmc3 = mmc3 %>% mutate(Gene=as.character(Gene)) %>% left_join(as.data.frame(org.Hs.egSYMBOL2EG), by=c("Gene"="symbol"))
+print("removing duplicated genes")
 
