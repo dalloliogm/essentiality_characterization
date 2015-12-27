af0e5aa83027a9b1aa7b5a9905f7630bb35c3d08
Giovanni M Dall'Olio <dalloliogm@gmail.com>
2015-12-27 12:56:07 +0100

    merging the two dataframes and plotting

--- src/compare_conservation.R
+++ src/compare_conservation.R
@@ -7,19 +7,43 @@ library(phastCons100way.UCSC.hg19)
 library(Homo.sapiens)
 
 
+
+
+print("reading S3 data")
+mmc3.raw = read.csv("data/mmc3.csv")
+# converting symbol to entrez - first attempt
+mmc3 = mmc3.raw %>% mutate(Gene=as.character(Gene)) %>% left_join(as.data.frame(org.Hs.egSYMBOL2EG), by=c("Gene"="symbol"))
+mmc3 %>% summarise(n_distinct(Gene), n_distinct(gene_id)) %>% print
+
+print("removing duplicated genes")
+# converting symbol to entrez - final attempt
+mmc3 = mmc3.raw %>%  
+    mutate(Gene=gsub("\\d(\\d)-Mar", "MARCH\\1", Gene)) %>% 
+    mutate(Gene=gsub("(\\d\\d)-Sep", "SEP\\1", Gene)) %>% 
+    mutate(Gene=gsub("(\\d\\d)-Dec", "DEC\\1", Gene)) %>%
+    left_join(as.data.frame(org.Hs.egALIAS2EG), by=c("Gene"="alias_symbol")) %>% 
+    filter(!duplicated(Gene)) 
+mmc3 %>% summarise(n_distinct(Gene), n_distinct(gene_id)) %>% print
+
+
+# getting gene coordinates
 if (!exists("allgenes")) {
-    allgenes = genes(TxDb.Hsapiens.UCSC.hg19.knownGene)
+    allgenes = genes(TxDb.Hsapiens.UCSC.hg19.knownGene) %>% 
+        subset(gene_id %in% mmc3$gene_id) 
+
     allgenes$cons100way = scores (phastCons100way.UCSC.hg19, allgenes)
 }
 
-allgenes.df = as.data.frame(mcols(allgenes))
-head(allgenes.df)
-allgenes.df = allgenes.df %>%
+#allgenes.df = as.data.frame(mcols(allgenes))
+#head(allgenes.df)
+allgenes.df = allgenes %>%
+    mcols %>%
+    as.data.frame %>% 
     arrange(desc(cons100way)) %>% 
-    mutate(rank=min_rank(cons100way))
+    mutate(rank=min_rank(cons100way)) %>%
+    left_join(mmc3, by="gene_id") %>% 
+    mutate(gene_type = ifelse(numTKOHits>2, "core", ifelse(numTKOHits>0, "fitness", "nonfitness")))
 
-print("reading S3 data")
-mmc3 = read.csv("data/mmc3.csv")
-mmc3 = mmc3 %>% mutate(Gene=as.character(Gene)) %>% left_join(as.data.frame(org.Hs.egSYMBOL2EG), by=c("Gene"="symbol"))
-print("removing duplicated genes")
+# plot
+allgenes.df %>% ggplot(aes(x=gene_type, y=-log(cons100way), fill=gene_type)) + geom_violin() + theme_bw() + ggtitle("conservation scores of core, fitness, and non-fitness genes")
 
